13af94e3f72098856f5a2eef4bebf0c4b93a1cff
Sergey Ivanichenko <murdalay@gmail.com>
2015-12-23 14:19:20 +0200

    price fix2

---
+++ common.blocks/enter/enter.deps.js
@@ -0,0 +1,11 @@
+[{
+    mustDeps : [
+        { block : 'i-bem', elems : ['dom'] }
+    ],
+    shouldDeps : [
+        { block : 'section-title' },
+        { block : 'inner-wrapper' },
+        { block : 'user-data' },
+        { block : 'error' }
+    ]
+}]
