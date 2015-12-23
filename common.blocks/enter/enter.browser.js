8bada8f3a0b893314d58628eaa7a11fba383aec5
Sergey Ivanichenko <murdalay@gmail.com>
2015-12-23 13:05:13 +0200

    price fix1

---
+++ common.blocks/enter/enter.browser.js
@@ -0,0 +1,13 @@
+modules.define('enter', ['i-bem__dom'], function(provide, BEMDOM) {
+
+provide(BEMDOM.decl(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+
+            }
+        }
+    }
+}));
+
+})
